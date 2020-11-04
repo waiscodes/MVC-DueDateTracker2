9af55a7fee8ace6fb2ae4aa4fd7cf844b792720e
waiscodes <b.wais@outlook.com>
2020-11-04 15:21:52 -0700

    added a new migration

--- LibraryDueDateTracker/Migrations/20201104222011_testMigration.Designer.cs
+++ LibraryDueDateTracker/Migrations/20201104222011_testMigration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using LibraryDueDateTracker.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LibraryDueDateTracker.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20201104222011_testMigration")]
+    partial class testMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
