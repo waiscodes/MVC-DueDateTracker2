acdf81b9f36f1f840c429ac1f200824110420415
James Grieve <jamesonrgrieve@gmail.com>
2020-11-05 09:21:21 -0700

    In-class debugging session;

--- LibraryDueDateTracker/Controllers/BookController.cs
+++ LibraryDueDateTracker/Controllers/BookController.cs
@@ -128,12 +128,15 @@ namespace LibraryDueDateTracker.Controllers
             List<Book> booksList;
             using (LibraryContext context = new LibraryContext())
             {
-                List<Book> bookWithoutBorrows = context.Books.Include(x => x.Author).ToList();
-                List<Borrow> borrowsList = context.Borrows.ToList();
+               booksList = context.Books.Include(x => x.Author).Include(x => x.Borrows).ToList();
+                // List<Borrow> borrowsList = context.Borrows.ToList();
+
+                // TODO: Add OrderBy to ensure LastOrDefault picks most recent.
+
                 //booksList = bookWithoutBorrows.Include(x => x.ID == borrowsList.LastOrDefault().BookID).ToList();
                 //Trying to figure out how to connect them together when one is a child element. 
 
-                booksList = bookWithoutBorrows;
+                // booksList = bookWithoutBorrows;
             }
             return booksList;
         }
